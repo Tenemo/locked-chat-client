beb345b7d8a08e752ab968d81b661e6cb63303e2
bax <dev.lasota@gmail.com>
2022-09-24 21:40:54 +0200

    redesign MessageInput.jsx to tsx

---
+++ src/components/chat/messageInput/MessageInput.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { formatISO } from "date-fns";
+import { IChat, TChatContextType } from "../../../context/DataContext";
+
+
+
+const MessageInput = ({ chat, addMessage }:TChatContextType):JSX.Element => {
+  
+  const [input, setInput] = useState<string>("");
+
+  const handleInputChange = (event:React.FormEvent<HTMLInputElement>) => {
+    setInput(event.currentTarget.value);
+  };
+  // const handleInputChange = ({ target: { value } }) => {
+  //   setInput(value);
+  // };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const newMessage:IChat = {
+      text: input,
+      nick: "unknown",
+      timeStamp: formatISO(Date.now()),
+    };
+    addMessage([...chat, newMessage]);
+
+    setInput("");
+  };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+    {/* <form> */}
+      <input
+        type="text"
+        name="inputValue"
+        value={input}
+        onChange={handleInputChange}
+      ></input>
+      <input type="submit" value="Submit"></input>
+    </form>
+  );
+};
+export default MessageInput;
