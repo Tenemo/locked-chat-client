6503949cd0a65e454016067869c44d9a33ef0eec
bax <dev.lasota@gmail.com>
2022-09-24 19:34:56 +0200

    redesign DataContext.jsx to tsx, added IChat Interface which defines the shape of message object, TChatContextType determines value assigned to the provider

---
+++ src/context/DataContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState } from "react";
+
+
+
+
+/**
+ * @timeStamp {string} must be in ISO-8601
+ */    
+interface IChat {
+  readonly text:string;
+  readonly timeStamp:number 
+  readonly nick:string
+}
+type TChatContextType = {
+  chat:IChat[];
+  addMessage:React.Dispatch<React.SetStateAction<IChat[]>>;
+}
+//React.ReactNode sprawdzić później
+type Props = {
+  children: JSX.Element,
+}
+//temporarily, the intended value will be assigned on the provider.
+const DataContext = createContext<TChatContextType | null>(null);
+
+export const DataProvider:React.FC<Props> = ({ children }) => {
+    const [chat, addMessage] = useState<IChat[]>([
+    {
+      text: "Lorem Ipsum is simply dummy text",
+      timeStamp: Date.now(),
+      nick: "unknown",
+    },
+    {
+      text: "Lorem Ipsum is simply dummy text",
+      timeStamp: Date.now(),
+      nick: "unknown",
+    },
+  ]);
+
+  return (
+    <DataContext.Provider
+      value={{
+        chat,
+        addMessage,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+export default DataContext;
--- src/test.tsx
+++
@@ -1,1 +0,0 @@
-console.log('testuje ustawienia tsx')
