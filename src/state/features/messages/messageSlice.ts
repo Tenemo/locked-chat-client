0e3608f2cdb5daa40de37a4d459959874b6704d4
bax <dev.lasota@gmail.com>
2022-09-26 20:42:57 +0200

    change name of reducer..

--- src/state/features/messages/messageSlice.ts
+++ src/state/features/messages/messageSlice.ts
@@ -18,11 +18,11 @@ export const messageSlice = createSlice({
   name: "messages",
   initialState,
   reducers: {
-    addMessageXD: (state, action: PayloadAction<Imessage>) => {
+    addMessage: (state, action: PayloadAction<Imessage>) => {
       state.chat = [...state.chat, action.payload];
     },
   },
 });
 
-export const { addMessageXD} = messageSlice.actions
+export const { addMessage} = messageSlice.actions
 export default messageSlice.reducer
