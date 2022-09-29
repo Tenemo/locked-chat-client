9cd5834f5133582d644048c36f6c8c1dd484cc71
bax <dev.lasota@gmail.com>
2022-09-29 02:19:22 +0200

    socket io

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,17 +1,34 @@
 import './chat.scss';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import MessageInput from './messageInput/MessageInput';
 import MessagesBox from './messagesBox/MessagesBox';
 
+import { startConnecting } from 'state/features/messages/messagesSlice';
 import { RootState } from 'state/store';
 
+let flag = false;
 const Chat = (): JSX.Element => {
-    const chat = useSelector((state: RootState) => state.messages.chat);
+    const messages = useSelector((state: RootState) => state.messages.messages);
+    const isEstablishingConnection = useSelector(
+        (state: RootState) => state.messages.isEstablishingConnection,
+    );
+    const isConnected = useSelector(
+        (state: RootState) => state.messages.isConnected,
+    );
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (!isConnected && !isEstablishingConnection && flag === false) {
+            flag = true;
+            console.log('tutaj');
+            dispatch(startConnecting());
+        }
+    }, [dispatch, isConnected, isEstablishingConnection]);
 
     return (
         <div className="chatContainer">
-            <MessagesBox messages={chat} />
+            <MessagesBox messages={messages} />
             <MessageInput />
         </div>
     );
--- src/state/features/messages/messagesTypes.ts
+++ src/state/features/messages/messagesTypes.ts
@@ -1,6 +1,15 @@
-export interface Message {
+export type Message = {
     readonly content: string;
     readonly timestamp: string;
     readonly author: string;
     readonly id: string;
+};
+export enum MessagesEvents {
+    NEW_MESSAGE = 'new-message',
+    NEW_MESSAGE_UPDATE = 'new-message-update',
 }
+export type MessagesState = {
+    messages: Message[];
+    isEstablishingConnection: boolean;
+    isConnected: boolean;
+};
